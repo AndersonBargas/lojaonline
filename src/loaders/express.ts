5cd6354b25aa4046e6dec6e9be31d78a395b9223
Anderson Bargas <anderson@andersonbargas.com>
2020-04-08 20:10:01 -0300

    Adicionando o middleware para interpretar o JSON.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 export default ({ app }: { app: express.Application }) => {
+
+    // middlewares
+    app.use(bodyParser.json());
 
 };
