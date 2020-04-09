9c4ee58b54c183d3c6d065ece7b1ce381b4afc64
Anderson Bargas <anderson@andersonbargas.com>
2020-04-09 09:24:50 -0300

    Tratando erro antes de enviar ao cliente.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 
 import apiRoutes from '../api';
 import configs from '../configs';
+import BadRequestError from '../errors/badRequestError';
 
 export default ({ app }: { app: express.Application }) => {
 
@@ -11,5 +12,22 @@ export default ({ app }: { app: express.Application }) => {
 
     // api routes
     app.use(configs.apiPrefix, apiRoutes());
+
+    // error handler
+    app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+        if (err.name === 'ValidationError') {
+            const newErr = new BadRequestError("Validation Error");
+            for (let field in err.errors) {
+                newErr.addFieldError({
+                    fieldName: err.errors[field].properties.path,
+                    errorDetail: `${err.errors[field].name}: ${err.errors[field].message}`,
+                });
+            }
+
+            return res.status(400).send(newErr).end();
+        } else {
+            return res.status(500).send().end();
+        }
+    });
 
 };
