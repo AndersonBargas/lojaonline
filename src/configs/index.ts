c8233fbcfc453cffeacf43008fa85bbc7f081a72
Anderson Bargas <anderson@andersonbargas.com>
2020-04-08 21:04:42 -0300

    Adicionando propriedade para a configuração da conexão com o MongoDB.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,5 @@
 export default {
     apiPrefix: '/api',
+    databaseURL: process.env.MONGODB_URL || 'mongodb://localhost:27017/lojaonline',
     port:  parseInt(process.env.PORT || '8000', 10),
 }
