3bcc002f20a7ec78328c14a24990943a7d9e6c8e
Anderson Bargas <anderson@andersonbargas.com>
2020-04-08 21:03:45 -0300

    Adicionando o await dentro de um método já que não consegui fazer funcionar fora de um método, pelo menos sem o babelJS.

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,19 @@ import configs from './configs';
 import loaders from './loaders'
 
 const app = express();
-loaders({ app: app });
 
 
-app.listen(configs.port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`Loja Online is ready on port #${configs.port}`);
-});
+async function appInit() {
+
+  await loaders({ app: app });
+
+  app.listen(configs.port, err => {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log(`Loja Online is ready on port #${configs.port}`);
+  });
+
+}
+
+appInit();
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import expressLoader from './express';
 
-export default ({ app }: { app: express.Application }) => {
+export default async ({ app }: { app: express.Application }) => {
+
     expressLoader({ app: app });
+
 };
