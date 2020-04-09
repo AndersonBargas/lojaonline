39b756b4510b453c5cd3c9a90a1028deaecebf55
Anderson Bargas <anderson@andersonbargas.com>
2020-04-09 15:21:32 -0300

    Propriedade de configuração para o segredo de assinatura dos tokens JWT.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,7 @@
 export default {
     apiPrefix: '/api',
     databaseURL: process.env.MONGODB_URL || 'mongodb://localhost:27017/lojaonline',
+    jwtSecret: process.env.JWT_SECRET || 'secret',
     maxResultsPerPage: 20,
     port:  parseInt(process.env.PORT || '8000', 10),
 }
