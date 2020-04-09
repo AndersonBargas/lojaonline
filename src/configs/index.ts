f8290fb5dcc36f96931a445890a14b8ae1b0c846
Anderson Bargas <anderson@andersonbargas.com>
2020-04-09 13:17:40 -0300

    Propriedade de configuração para a quantidade máxima de retornos por página.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,6 @@
 export default {
     apiPrefix: '/api',
     databaseURL: process.env.MONGODB_URL || 'mongodb://localhost:27017/lojaonline',
+    maxResultsPerPage: 20,
     port:  parseInt(process.env.PORT || '8000', 10),
 }
