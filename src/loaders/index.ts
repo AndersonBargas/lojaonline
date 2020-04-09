2467eb81c6e9385492c8f5986d58410690188e30
Anderson Bargas <anderson@andersonbargas.com>
2020-04-09 10:30:15 -0300

    Conexão ao banco mongoDB.

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import expressLoader from './express';
+import mongooseLoader from './mongoose';
 
 export default async ({ app }: { app: express.Application }) => {
 
     expressLoader({ app: app });
+
+    try {
+        const mongoConnection = await mongooseLoader();
+    } catch (err) {
+        throw err;
+    }
+    
 
 };
